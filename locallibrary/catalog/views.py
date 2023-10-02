3fda558ee5730654a1bdff776a4d50c8bdca476a
tonsby <galante.tkachenko86@gmail.com>
2023-10-02 20:37:39 +0300

    Работает http://127.0.0.1:8000/catalog/author/1/update/

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -135,8 +135,7 @@ class AuthorCreate(CreateView):
     initial={'date_of_death':'12/10/2016',}
 
 def author_update(request, pk):
-    author = get_object_or_404(Author,id=pk)
-    author_id = Book.objects.filter(id=pk).first()
+    author = get_object_or_404(Author,pk=pk)
 
     if request.method == 'POST':
 
@@ -147,6 +146,7 @@ def author_update(request, pk):
             # Обработка данных из form.cleaned_data
             # (здесь мы просто присваиваем их полю due_back)
             author.date_of_death = form.cleaned_data['update_author_date_of_death']
+            author.first_name = ['update_author_first_name']
             author.save()
 
             # Переход по адресу 'authors':
@@ -158,7 +158,7 @@ def author_update(request, pk):
         form = UpdateAuthorForm(initial={'update_author_date_of_death': proposed_date_of_death, })
 
     return render(request, 'catalog/book_update_authors.html',
-                  {'form': form, 'authors': author, 'author_id': author_id})  # render объединяет запрос со словарём
+                  {'form': form, 'authors': author})  # render объединяет запрос со словарём
 
 
     # ФОРМЫ ДЛЯ АВТОРОВ, НЕОБХОДИМО ИМПОРТИРОВАТЬ Create, Update, Delete
